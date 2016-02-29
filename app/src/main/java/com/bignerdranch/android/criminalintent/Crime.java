6c33d3aabc92b1d0ac64fc4ea1d33c2d73b2fcd1
TimPaczynski <tpaczyns@gustavus.edu>
2016-02-28 23:58:09 -0600

    challenge complete

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.UUID;
 
 /**
@@ -11,11 +14,14 @@ public class Crime {
     private  String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private SimpleDateFormat sdf;
 
     public Crime(){
         //generate unique crime identifier
         mId = UUID.randomUUID();
         mDate = new Date();
+        sdf = new SimpleDateFormat("EEEE, LLLL d, y", Locale.US);
+
     }
 
     public UUID getId() {
@@ -40,6 +46,9 @@ public class Crime {
         return mSolved;
     }
 
+    public String crimeDate() {
+        return sdf.format(mDate);
+    }
     public void setSolved(boolean solved) {
         mSolved = solved;
     }
